19ae39ab7a45decef904a2f1d94417e61147b507
Jan Świątek <swiatek-jan@wp.pl>
2023-11-22 22:05:20 +0100

    Zad 1 i 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector("ul.gallery");
 
+
+// const markup = galleryItems.map((image) => `<div><a href="${image.preview}"><img src="${image.preview}" alt="${image.description}"/></a></div>`).join("");
+const markup = galleryItems.map((image) => `<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`).join("");
+gallery.insertAdjacentHTML('beforeend', markup);
+// button.addEventListener("click", selestedImage);
+
+
+gallery.onclick = (event) => {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+  function onEscape(event) {
+    if (event.code !== "Escape") return;
+    instance.close();
+  }
+
+  const instance = basicLightbox.create(
+    `
+		<img width="1200" src= ${event.target.dataset.source}>
+	`,
+    {
+      onShow: () => window.addEventListener("keydown", onEscape),
+      onClose: () => window.removeEventListener("keydown", onEscape),
+    }
+  );
+  instance.show();
+};
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector("ul.gallery");
+
+const markup = galleryItems.map((image) => 
+`<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+).join("");
+gallery.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    close: true,
+});
+
+
+
+lightbox.on("show.simplelightbox");
 console.log(galleryItems);
